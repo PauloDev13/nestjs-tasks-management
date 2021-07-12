f73b4f64371fdb0de4c86cc9dc78e814021fefab
PauloDev <devpgm2020@gmail.com>
2021-07-12 18:43:24 -0300

    Adding createTasks endpoint to Task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { TasksModel } from './tasks.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {
-    this.tasksService.getAllTasks();
+  getAllTasks(): TasksModel[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): TasksModel {
+    return this.tasksService.createTask(title, description);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
+import { TasksModel, TaskStatus } from './tasks.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: TasksModel[] = [];
 
-  getAllTasks() {
+  getAllTasks(): TasksModel[] {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): TasksModel {
+    const task: TasksModel = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
   }
 }
