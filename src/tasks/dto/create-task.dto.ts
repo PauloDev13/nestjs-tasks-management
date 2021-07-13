b59ac6186b4837e0a5b6594a1d501cc1fc9864f1
PauloDev <devpgm2020@gmail.com>
2021-07-12 23:15:54 -0300

    Implementation ValidationPipes for task

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsNotEmpty({ message: 'Informe o título da tarefa.' })
   title: string;
+
+  @IsNotEmpty({ message: 'Informe a descrição da tarefa.' })
   description: string;
 }
