68a8e563037b28fbf78c5aff036616eee5655c5c
PauloDev <devpgm2020@gmail.com>
2021-07-14 17:53:46 -0300

    Error Handling: Username conflict

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,6 +1,10 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(UserEntity)
 export class UsersRepository extends Repository<UserEntity> {
@@ -11,6 +15,17 @@ export class UsersRepository extends Repository<UserEntity> {
       username,
       password,
     });
-    await this.save(userCreated);
+    try {
+      await this.save(userCreated);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException(null, 'Usuário já cadastrado.');
+      } else {
+        throw new InternalServerErrorException(
+          null,
+          'Ocorreu um erro inesperado.',
+        );
+      }
+    }
   }
 }
